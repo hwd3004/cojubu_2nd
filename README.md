be162aa4420a1e5b943321ad5073e95a8bc33db7
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-10 20:44:15 +0900

    2. 일단 먼저 배포와 호스팅 해보기

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 npm install --save firebase
 
 ---
+
+# 2. 일단 먼저 배포와 호스팅 해보기
+
+참고 유튜브 - https://youtu.be/RyhG1L0dCZ8
+
+내 사이트 주소 - https://coju-bu.web.app/
+
+---
--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       코주부
+      <script src="/__/firebase/7.23.0/firebase-app.js"></script>
+      <script src="/__/firebase/7.23.0/firebase-analytics.js"></script>
+      <script src="/__/firebase/init.js"></script>
     </div>
   );
 }
